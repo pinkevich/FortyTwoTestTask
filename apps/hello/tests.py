6663c2754b92f5e7a099776140ff7f74b9984faa
Alexander <my@pinkevich.net>
2015-09-14 16:49:52 +0300

    ticket:1 Initial commit fix tests and add linebreaks for bio and other contacts

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -8,7 +8,7 @@ class HelloTests(TestCase):
 
     def test_main_page(self):
         """
-        test main page (opened, template, context)
+        Test main page (opened, template, context)
         """
         bio = Bio.objects.first()
         url = reverse_lazy('hello:main')
@@ -17,17 +17,16 @@ class HelloTests(TestCase):
         self.assertEqual(Bio.objects.count(), 1)
         self.assertEqual(resp.context['bio'], bio)
         self.assertTemplateUsed(resp, 'main.html')
-        self.assertContains(resp, 'Alexander')
-        self.assertContains(resp, 'Pinkevich')
-        self.assertContains(resp, 'March 15, 1993')
-        self.assertContains(resp, 'my@pinkevich.net')
-        self.assertContains(resp, 'pinkevich@jabber.ru')
-        self.assertContains(resp, 'pinkevich.net')
+        self.assertContains(resp, bio.first_name)
+        self.assertContains(resp, bio.last_name)
+        self.assertContains(resp, bio.date_of_birth.strftime('r'))
+        self.assertContains(resp, bio.email)
+        self.assertContains(resp, bio.jabber)
+        self.assertContains(resp, bio.skype)
 
-    def test_main_page_errors(self):
+    def test_main_page_more_one_bio(self):
         """
-        test main page if more than two objects in bio model
-        and if not objects in bio model
+        Test main page if more than two objects in bio model
         """
         bio = Bio.objects.first()
         new_bio = Bio.objects.create(first_name='test_first_name',
@@ -41,7 +40,7 @@ class HelloTests(TestCase):
         self.assertEqual(Bio.objects.count(), 2)
         self.assertEqual(resp.context['bio'], bio)
         self.assertNotEqual(resp.context['bio'], new_bio)
-        self.assertContains(resp, 'Alexander')
+        self.assertContains(resp, bio.first_name)
         self.assertNotContains(resp, 'test_first_name')
         self.assertNotContains(resp, 'test_last_name')
         self.assertNotContains(resp, 'January 01, 2000')
@@ -49,11 +48,14 @@ class HelloTests(TestCase):
         self.assertNotContains(resp, 'test')
         self.assertTemplateUsed(resp, 'main.html')
 
+    def test_main_page_not_bio(self):
+        """
+        If not objects in bio model
+        """
         Bio.objects.all().delete()
+        url = reverse_lazy('hello:main')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(Bio.objects.count(), 0)
         self.assertEqual(resp.context['bio'], None)
-        self.assertNotContains(resp, 'Alexander')
-        self.assertNotContains(resp, 'test_first_name')
         self.assertTemplateUsed(resp, 'main.html')
