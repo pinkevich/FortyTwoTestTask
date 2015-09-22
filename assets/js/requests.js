e406b37897d74153a2ddd2da4c5c3147ef5e7081
Alexander <my@pinkevich.net>
2015-09-22 19:23:01 +0300

    ticket:3 fix requests.js

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var titleCount = 0;
     var isChangedTitle = false;
     var allRequests = [];
+    var tempRequests = [];
 
     function getRequests() {
         $.ajax({
@@ -18,9 +19,13 @@ $(document).ready(function () {
                         if (!item.fields.is_read) {
                             titleCount += 1;
                         }
-                        allRequests.push(item)
+                        allRequests.push(item);
+                        tempRequests.push(item);
                     }
                 });
+            },
+            complete: function () {
+                isRead()
             }
         });
         if (isChangedTitle && titleCount) {
@@ -35,27 +40,31 @@ $(document).ready(function () {
         isChangedTitle = true;
     });
 
-    function isRead(pk) {
-        $.ajax({
-            url: '/requests/',
-            type: 'POST',
-            dataType: 'json',
-            data: {csrfmiddlewaretoken: csrftoken, request_pk: pk},
-            success: function (response) {
-                if (response.success) {
-                    titleCount -= 1;
-                }
-            }
-        });
+    function isRead() {
+        if (!isChangedTitle) {
+            _.each(tempRequests, function (item) {
+                $.ajax({
+                    url: '/requests/',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {csrfmiddlewaretoken: csrftoken, request_pk: item.pk},
+                    success: function (response) {
+                        if (response.success) {
+                            titleCount -= 1;
+                            $('#request-' + item.pk).find('.is-new').remove();
+                        }
+                    }
+                });
+            });
+            tempRequests = [];
+        }
     }
 
     $('body').on('click', '.request-simple', function (e) {
         var target = e.toElement || e.relatedTarget || e.target;
-        $(target).find('.is-new').remove();
-        var requestId = $(target).data('id');
         var requestsBody = $('.requests-body');
         _.find(allRequests, function (obj) {
-            if (obj.pk == requestId) {
+            if (obj.pk == $(target).data('id')) {
                 requestsBody.children().remove();
                 requestsBody.append(
                     '<div>' +
@@ -67,7 +76,6 @@ $(document).ready(function () {
                     '<p>' + obj.fields.header + '</p>' +
                     '</div>'
                 );
-                isRead(requestId)
             }
         });
     });
@@ -75,5 +83,5 @@ $(document).ready(function () {
     getRequests();
     setInterval(function () {
         getRequests();
-    }, 1500);
+    }, 1000);
 });
