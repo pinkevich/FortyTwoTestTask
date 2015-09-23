3cc3b7715bd4298b39dcebcd87b7e5607b37eb36
Alexander <my@pinkevich.net>
2015-09-23 11:52:10 +0300

    Merge branch 't8_tag' into t13_priority
    # Conflicts:
    #	apps/hello/signals.py

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -1,6 +1,5 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
-from django.db.utils import OperationalError
 
 from .models import History
 
@@ -8,24 +7,17 @@ from .models import History
 @receiver(post_save)    # noqa
 def create_or_update_object(sender, instance, created, **kwargs):
     if sender != History:
-        try:
-            history = History(model_name=instance._meta.model_name,
-                              model_instance=instance)
-            if created:
-                history.action = History.CREATED
-            else:
-                history.action = History.EDITED
-            history.save()
-        except OperationalError:
-            pass
+        history = History(model_name=instance._meta.model_name,
+                          model_instance=instance)
+        history.action = History.EDITED
+        if created:
+            history.action = History.CREATED
+        history.save()
 
 
 @receiver(post_delete)  # noqa
 def delete_object(sender, instance, **kwargs):
     if sender != History:
-        try:
-            History.objects.create(model_name=instance._meta.model_name,
-                                   model_instance=instance,
-                                   action=History.DELETED)
-        except OperationalError:
-            pass
+        History.objects.create(model_name=instance._meta.model_name,
+                               model_instance=instance,
+                               action=History.DELETED)
